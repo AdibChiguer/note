7000d6f022566170db2e0ab0519d7a72d6641c16
adib <chigueradib9@gmail.com>
2023-05-27 15:16:01 +0100

    add responsive

--- script.js
+++ script.js
@@ -9,15 +9,50 @@ let textContent = document.getElementById("note-content");
 let mood = "create";
 let tmp;
 
+let menuBtn = document.getElementById('menu-btn');
+let xmenuBtn = document.getElementById('xmenu-btn');
+let h = document.getElementById('h');
+let noteMEnu = document.querySelector('.notes-menu');
+
+if(window.innerWidth <= 1000){
+    menuBtn.classList.remove('hedde');
+    xmenuBtn.classList.remove('hedde');
+    h.innerHTML = '';
+}
+window.addEventListener('resize',function addMenuBtn(){
+    if(window.innerWidth <= 1000){
+        menuBtn.classList.remove('hedde');
+        xmenuBtn.classList.remove('hedde');
+        h.innerHTML = '';
+    }else{
+        menuBtn.classList.add('hedde');
+        xmenuBtn.classList.add('hedde');
+        h.innerHTML = 'writing area';
+    }
+})
+menuBtn.addEventListener('click', function showSideMenu(){
+    noteMEnu.classList.add('active-menu');
+    noteMEnu.classList.remove('close-menu');
+    console.log('hi');
+})
+xmenuBtn.addEventListener('click', function showSideMenu(){
+    noteMEnu.classList.add('close-menu');
+    noteMEnu.classList.remove('active-menu');
+    console.log('by');
+})
+
 plusBtn.onclick = function (){
     svg.classList.add("heddin-show-area");
     typingArea.classList.remove("heddin-text-typing-area");
     title.style.display = "block";
-    changeBtnContainer.style.display = "block";
+    changeBtnContainer.style.display = "flex";
     changeBtnContainer.innerHTML = `<button id="save" onclick="save()" ><i class="fa-sharp fa-solid fa-floppy-disk"></i></button>`;
     title.value = '';
     textContent.value = '';
-
+    if(window.innerWidth <= 1000){
+        noteMEnu.classList.add('close-menu');
+        noteMEnu.classList.remove('active-menu');
+    }
 }
 
 let noteData;
@@ -73,12 +108,17 @@ function openNote(i){
     svg.classList.add("heddin-show-area");
     typingArea.classList.remove("heddin-text-typing-area");
     title.style.display = "block";
-    changeBtnContainer.style.display = "block";
+    changeBtnContainer.style.display = "flex";
     changeBtnContainer.innerHTML = `<button id="edite" onclick="editeNote(${i})"><i class="fa-sharp fa-solid fa-pen-to-square"></i></button>
     <button id="delete" onclick="deleteNote(${i})" ><i class="fa-sharp fa-solid fa-trash-can"></i></button>`;
     
     title.value = noteData[i].title;
     textContent.value = noteData[i].note;
+
+    if(window.innerWidth <= 1000){
+        noteMEnu.classList.add('close-menu');
+        noteMEnu.classList.remove('active-menu');
+    }
 }
 
 function editeNote(i){
